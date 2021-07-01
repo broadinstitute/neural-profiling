9502b6113407042f4050302aee865b8839e8a0a9
michaelbornholdt <michael.bornholdt@outlook.com>
2021-07-01 11:51:40 -0400

    aggregation script

--- pre-trained/efficient_net/aggregated/run_aggregation.py
+++ pre-trained/efficient_net/aggregated/run_aggregation.py
@@ -9,19 +9,22 @@ import pandas as pd
 from pycytominer.cyto_utils.DeepProfiler_processing import AggregateDeepProfiler
 
 project_dir = os.path.join(
-    os.path.dirname(__file__), "..", "features1"
+    os.path.dirname(__file__)
 )
 
 profile_dir = os.path.join(project_dir, "outputs", "results", "features")
 
 index_file = os.path.join(project_dir, "inputs", "metadata", "sub_index.csv")
 
+output_folder = os.path.join(project_dir, 'outputs',"results",'aggregated')
+
 well_class = AggregateDeepProfiler(
     index_file=index_file,
     profile_dir=profile_dir,
     aggregate_operation="median",
     aggregate_on="well",
+    output_file=output_folder,
 )
-df_well = well_class.annotate_deep()
+df_well = well_class.aggregate_deep()
 
-df_well.to_csv('final.csv')
+df_well.to_csv('aggregated_resnet_median.csv')
